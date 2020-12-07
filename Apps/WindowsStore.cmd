9d0923d97462dc1056b1d489ec0be7650d67dff7
SteavenDev <62666559+omartube706@users.noreply.github.com>
2020-12-08 00:07:02 +0200

    Update WindowsStore.cmd

--- Apps/WindowsStore.cmd
+++ Apps/WindowsStore.cmd
@@ -2,6 +2,7 @@ echo off
 cls
 title Install WindowsStore
 powershell.exe "Get-AppxPackage -allusers Microsoft.WindowsStore | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}"
+powershell.exe "Get-AppxPackage -allusers Microsoft.WindowsStore"
 @pause
 cd..
 SteavenToolBox.cmd
