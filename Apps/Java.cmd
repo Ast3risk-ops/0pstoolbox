28f2d70b4db5280da3edfab91819507708c9bd4c
SteavenDev <62666559+omartube706@users.noreply.github.com>
2020-12-08 00:20:42 +0200

    Update Java.cmd

--- Apps/Java.cmd
+++ Apps/Java.cmd
@@ -3,6 +3,7 @@ cls
 title Install Java
 powershell.exe "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
 choco install jre8 -y
+choco install jdk8 -y
 @pause
 cd..
 SteavenToolBox.cmd
