a2d57760e1f814fc9a202e682daf6a95b187adb2
SteavenDev <62666559+omartube706@users.noreply.github.com>
2020-12-16 18:04:01 +0200

    Update ToolBox.ps1

--- ToolBox.ps1
+++ ToolBox.ps1
@@ -1,2 +1,2 @@
 Invoke-WebRequest https://raw.githubusercontent.com/omartube706/SteavenToolBox/main/SteavenToolBox.cmd -OutFile C:\windows\temp\SteavenToolBox.cmd
-start-process "cmd.exe" "/c C:\windows\temp\SteavenToolBox.cmd"
+C:\windows\temp\SteavenToolBox.cmd
