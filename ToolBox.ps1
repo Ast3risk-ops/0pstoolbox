4b878e4e706c277501e857e665aa32101a23fe78
SteavenDev <62666559+omartube706@users.noreply.github.com>
2020-12-17 14:27:58 +0200

    updated

--- ToolBox.ps1
+++ ToolBox.ps1
@@ -1,3 +1,4 @@
-Invoke-WebRequest https://raw.githubusercontent.com/omartube706/SteavenToolBox/main/SteavenToolBox.cmd -OutFile C:\windows\temp\SteavenToolBox.cmd
+Invoke-WebRequest https://raw.githubusercontent.com/omartube706/SteavenToolBox/main/SteavenToolBox.zip -OutFile C:\windows\temp\SteavenToolBox.zip
+Expand-Archive -LiteralPath C:\windows\temp\SteavenToolBox.zip -DestinationPath C:\windows\temp\ InvoicesUnzipped
 
 powershell.exe "C:\windows\temp\SteavenToolBox.cmd"
