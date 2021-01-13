6e859486d32d95c02ec201465f4439d9e4e8bc0b
MuazAhmed12345 <77384984+MuazAhmed12345@users.noreply.github.com>
2021-01-13 13:47:53 +0400

    Update SteavenToolBox.cmd

--- SteavenToolBox.cmd
+++ SteavenToolBox.cmd
@@ -197,7 +197,7 @@ echo 2. AnyDesk                  12. Java SE Runtime Environment 8
 echo 3. Audacity                 13. Notepad++
 echo 4. Blender                  14. VLC
 echo 5. "C++ RunTime"            15. Pyhton
-echo 6. MS Office
+echo 6. MS Office                16.Office 365 Business
 echo 7. netframwork 3
 echo 8. winrar
 echo 9. DirectX
@@ -223,6 +223,7 @@ if '%choice%'=='12' choco install jre8 -y
 if '%choice%'=='13' choco install notepadplusplus.install -y
 if '%choice%'=='14' choco install vlc -y
 if '%choice%'=='15' choco install python3 -y
+if '%choice%'=='16' choco install office365business -y
 if '%choice%'=='0' goto start
 ECHO.
 goto 11
