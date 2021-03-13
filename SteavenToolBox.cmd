b5270e889d4f2bf7f0a4eb94edfe93b98a665cb4
DoesSoulEverDie <sdude743@gmail.com>
2021-03-13 15:50:16 +0200

    Update SteavenToolBox.cmd

--- SteavenToolBox.cmd
+++ SteavenToolBox.cmd
@@ -186,7 +186,7 @@ echo 11. Adobe Acrobat Reader DC 27. Razer Cortex
 echo 12. Java SE Runtime Environment 8  28. ApowerMirror
 echo 13. Notepad++                    29. Aimp 
 echo 14. VLC                          30. Google Chrome
-echo 15. Pyhton
+echo 15. Pyhton                       31. Google Drive
 echo 16. Office 365 Business
 echo to Request More say for me at discord SteavenGamerYT#2247 and AlphaReturns#7496 and his helper SoulNeverDies4#5171
 echo ---------------------------------------------------------------------------------------------------------------------
@@ -224,6 +224,7 @@ if '%choice%'=='27' choco install gamebooster -y
 if '%choice%'=='28' start https://github.com/SteavenGamerYT/SteavenToolBox/raw/main/apowermirror-setup.exe
 if '%choice%'=='29' choco install aimp -y
 if '%choice%'=='30' choco install googlechrome  google-translate-chrome save-to-google-drive-chrome -y
+if '%choice%'=='31' choco install googledrive -y
 if '%choice%'=='0' goto start
 ECHO.
 goto 11
