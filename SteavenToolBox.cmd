dfb4a2ec5b0eba17f7409c49db843be14784b9b4
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2021-04-22 16:10:00 +0200

    test

--- SteavenToolBox.cmd
+++ SteavenToolBox.cmd
@@ -207,7 +207,7 @@ echo 45. Dropbox                      46. Virtualbox
 echo 47. Winaero Tweaker              48. Iobit Program Pack
 echo 49. Process Explorer             50. WinDirStat
 echo 51. Everything                   52. Visual Studio 2017 Build Tools
-echo 53. Adobe Shockwave Player
+echo 53. Adobe Shockwave Player       54. Roblox
 echo to Request More say for me at discord SteavenGamerYT#2247 and AlphaReturns#7496 and his helper SoulNeverDies4#5171
 echo ---------------------------------------------------------------------------------------------------------------------
 echo 0. Back to menu
@@ -267,6 +267,7 @@ if '%choice%'== '50' choco install windirstat -y
 if '%choice%'== '51' choco install everything
 if '%choice%'== '52' choco install visualstudio2017buildtools
 if '%choice%'== '53' choco install adobeshockwaveplayer
+if '%choice%'== '54' aria2c https://github.com/omartube706/SteavenToolBox/raw/main/roblox.exe & start /wait roblox.exe
 if '%choice%'=='0' goto start
 ECHO.
 goto 11
