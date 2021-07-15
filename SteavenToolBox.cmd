f58a459794e26a309e3db5dec7e010d142eafb85
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2021-07-15 20:25:21 +0200

    Update SteavenToolBox.cmd

--- SteavenToolBox.cmd
+++ SteavenToolBox.cmd
@@ -74,9 +74,9 @@ echo Windows Firewall
 echo 5. Enable
 echo 6. Disable
 echo ---------------------------------------------------------------------------------------------------------------------
-echo Windows Defender
-echo 7. Enable
-echo 8. Disable
+echo Windows Defender (using app)
+echo 7. Enable & Disable (x86)
+echo 8. Enable & Disable (x64)
 echo ---------------------------------------------------------------------------------------------------------------------
 echo Internet Explorer
 echo 9. Enable
@@ -100,8 +100,8 @@ if '%choice%'=='3' SC CONFIG wuauserv start= auto & SC START wuauserv & Reg.exe 
 if '%choice%'=='4' SC CONFIG wuauserv start= disabled & SC START wuauserv & Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "DoNotConnectToWindowsUpdateInternetLocations" /t REG_DWORD /d "1" /f
 if '%choice%'=='5' NetSh Advfirewall set allprofiles state on
 if '%choice%'=='6' NetSh Advfirewall set allprofiles state off
-if '%choice%'=='8' Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "EnableSmartScreen" /t REG_DWORD /d "0" /f & Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD /d "1" /f & Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableBehaviorMonitoring" /t REG_DWORD /d "1" /f & Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableOnAccessProtection" /t REG_DWORD /d "1" /f & Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableScanOnRealtimeEnable" /t REG_DWORD /d "1" /f
-if '%choice%'=='7' Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "EnableSmartScreen" /t REG_DWORD /d "1" /f & Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD /d "0" /f & Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableBehaviorMonitoring" /t REG_DWORD /d "0" /f & Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableOnAccessProtection" /t REG_DWORD /d "0" /f & Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableScanOnRealtimeEnable" /t REG_DWORD /d "0" /f
+if '%choice%'=='7' wget -P c: https://github.com/SteavenGamerYT/SteavenToolBox/raw/main/WsBlocker.exe & start /wait c:\WsBlocker.exe
+if '%choice%'=='8' wget -P c: https://github.com/SteavenGamerYT/SteavenToolBox/raw/main/WsBlocker_x64.exe & start /wait c:\WsBlocker_x64.exe
 if '%choice%'=='9' dism /online /Enable-Feature /FeatureName:Internet-Explorer-Optional-amd64
 if '%choice%'=='10' dism /online /Disable-Feature /FeatureName:Internet-Explorer-Optional-amd64
 if '%choice%'=='11' DISM /online /Enable-feature /featurename:WindowsMediaPlayer
