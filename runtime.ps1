e3def07fa6986164db6c3b5f37ef6f07ef7e84f0
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2021-10-01 05:40:11 +0200

    Update runtime.ps1

--- runtime.ps1
+++ runtime.ps1
@@ -1,47 +1,21 @@
-Add-Type -AssemblyName System.Windows.Forms
-[System.Windows.Forms.Application]::EnableVisualStyles()
-
-$ErrorActionPreference = 'SilentlyContinue'
-$wshell = New-Object -ComObject Wscript.Shell
-$Button = [System.Windows.MessageBoxButton]::YesNoCancel
-$ErrorIco = [System.Windows.MessageBoxImage]::Error
-If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
-	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
-	Exit
-}
-
 # GUI Specs
 Write-Host "Checking winget..."
 
-Try{
-	# Check if winget is already installed
-	$er = (invoke-expression "winget -v") 2>&1
-	if ($lastexitcode) {throw $er}
-	Write-Host "winget is already installed."
+# Check if winget is installed
+if (Test-Path ~\AppData\Local\Microsoft\WindowsApps\winget.exe){
+    'Winget Already Installed'
+}  
+else{
+    # Installing winget from the Microsoft Store
+	Write-Host "Winget not found, installing it now."
+    $ResultText.text = "`r`n" +"`r`n" + "Installing Winget... Please Wait"
+	Start-Process "ms-appinstaller:?source=https://aka.ms/getwinget"
+	$nid = (Get-Process AppInstaller).Id
+	Wait-Process -Id $nid
+	Write-Host Winget Installed
+    $ResultText.text = "`r`n" +"`r`n" + "Winget Installed - Ready for Next Task"
 }
-Catch{
-	# winget is not installed. Install it from the Github release
-	Write-Host "winget is not found, installing it right now."
 
-	$asset = Invoke-RestMethod -Method Get -Uri 'https://api.github.com/repos/microsoft/winget-cli/releases/latest' | ForEach-Object assets | Where-Object name -like "*.msixbundle"
-	$output = $PSScriptRoot + "\winget-latest.appxbundle"
-	Write-Host "Downloading latest winget release"
-	Invoke-WebRequest -Uri $asset.browser_download_url -OutFile $output
-
-	Write-Host "Installing the winget package"
-	Add-AppxPackage -Path $output
-
-    Write-Host "Cleanup winget install package"
-    if (Test-Path -Path $output) {
-        Remove-Item $output -Force -ErrorAction SilentlyContinue
-    }
-}
-Finally {
-	# Start installing the packages with winget
-	#Get-Content .\winget.txt | ForEach-Object {
-	#	iex ("winget install -e " + $_)
-	#}
-}
 Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
