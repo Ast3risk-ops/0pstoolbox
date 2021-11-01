3fb13ea7c39a591a23f8c6d0f5a797992cab59e8
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2021-11-01 13:53:18 +0200

    fixed choco spam bug

--- runtime.ps1
+++ runtime.ps1
@@ -33,4 +33,3 @@ iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 scoop install aria2 wget git
 scoop update
-choco upgrade
