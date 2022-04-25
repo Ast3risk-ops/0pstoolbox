4fd0f52b7bb7d23ea4ea90d2c7fbbcd697d6b4ce
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2022-04-25 17:45:02 +0200

    t

--- ToolBox.ps1
+++ ToolBox.ps1
@@ -1,4 +1,4 @@
 Invoke-WebRequest https://raw.githubusercontent.com/omartube706/SteavenToolBox/main/SteavenToolBox.cmd -OutFile C:\windows\temp\SteavenToolBox.cmd
-Invoke-WebRequest https://raw.githubusercontent.com/omartube706/SteavenToolBox/main/RunTime-no-admin.cmd -OutFile C:\windows\temp\RunTime-no-admin.cmd
-cmd /k "C:\windows\temp\RunTime-no-admin.cmd"
+Invoke-WebRequest https://raw.githubusercontent.com/omartube706/SteavenToolBox/main/RunTime-no-admin.cmd -OutFile C:\windows\temp\RunTime-no-admin.ps1
+cmd /k "C:\windows\temp\RunTime-no-admin.ps1"
 powershell.exe "C:\windows\temp\SteavenToolBox.cmd"
