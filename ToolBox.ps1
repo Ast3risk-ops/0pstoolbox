d4e8a2d1696154151dd821b27a228bcbef58c409
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2022-04-25 13:22:10 +0200

    Update ToolBox.ps1

--- ToolBox.ps1
+++ ToolBox.ps1
@@ -1,4 +1,4 @@
 Invoke-WebRequest https://raw.githubusercontent.com/omartube706/SteavenToolBox/main/SteavenToolBox.cmd -OutFile C:\windows\temp\SteavenToolBox.cmd
 Invoke-WebRequest https://raw.githubusercontent.com/omartube706/SteavenToolBox/main/RunTime-no-admin.cmd -OutFile C:\windows\temp\RunTime-no-admin.cmd
-powershell.exe "C:\windows\temp\RunTime-no-admin.cmd"
-sudo powershell.exe "C:\windows\temp\SteavenToolBox.cmd"
+cmd /k "C:\windows\temp\RunTime-no-admin.cmd"
+powershell.exe "C:\windows\temp\SteavenToolBox.cmd"
