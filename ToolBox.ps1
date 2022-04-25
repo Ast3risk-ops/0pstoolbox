af0132612f85591c6345ad062ae5aa813febb969
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2022-04-25 17:53:13 +0200

    Update ToolBox.ps1

--- ToolBox.ps1
+++ ToolBox.ps1
@@ -1,5 +1,5 @@
 Invoke-WebRequest https://raw.githubusercontent.com/omartube706/SteavenToolBox/main/SteavenToolBox.cmd -OutFile C:\windows\temp\SteavenToolBox.cmd
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-iwr -useb get.scoop.sh | iex
+start cmd /k "powershell -command "iwr -useb get.scoop.sh | iex""
 scoop install sudo aria2 wget git
 sudo powershell.exe "C:\windows\temp\SteavenToolBox.cmd"
