d7ca7f5c1ba1bbd40a4070a10497ff681a6d953f
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2022-04-25 17:46:59 +0200

    trying to fix runtime

--- RunTime-no-admin.ps1
+++
@@ -1,4 +0,0 @@
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-iwr -useb get.scoop.sh | iex
-scoop install sudo aria2 wget git
-exit
--- ToolBox.ps1
+++ ToolBox.ps1
@@ -1,4 +1,5 @@
 Invoke-WebRequest https://raw.githubusercontent.com/omartube706/SteavenToolBox/main/SteavenToolBox.cmd -OutFile C:\windows\temp\SteavenToolBox.cmd
-Invoke-WebRequest https://raw.githubusercontent.com/omartube706/SteavenToolBox/main/RunTime-no-admin.cmd -OutFile C:\windows\temp\RunTime-no-admin.ps1
-cmd /k "C:\windows\temp\RunTime-no-admin.ps1"
-powershell.exe "C:\windows\temp\SteavenToolBox.cmd"
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+iwr -useb get.scoop.sh | iex
+scoop install sudo aria2 wget git
+sudo powershell.exe "C:\windows\temp\SteavenToolBox.cmd"
