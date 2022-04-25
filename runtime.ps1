4d055e11e0d605562bd39154ed62ad4db6817467
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2022-04-25 13:19:16 +0200

    trying to fix runtime

--- runtime.ps1
+++ runtime.ps1
@@ -30,5 +30,3 @@ else{
 
 Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
-scoop install aria2 wget git
