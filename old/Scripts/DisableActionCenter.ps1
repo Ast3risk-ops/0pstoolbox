15e6011a01c8b8f6bd988ec1a3aa8bba151b5bfb
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2023-03-21 18:09:16 +0200

    ah

--- old/Scripts/DisableActionCenter.ps1
+++
@@ -1,7 +0,0 @@
-# Disable Action Center
- Write-Host "Disabling Action Center..."
- If (!(Test-Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer")) {
- New-Item -Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer" | Out-Null
- }
- Set-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer" -Name "DisableNotificationCenter" -Type DWord -Value 1
- Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\PushNotifications" -Name "ToastEnabled" -Type DWord -Value 0
