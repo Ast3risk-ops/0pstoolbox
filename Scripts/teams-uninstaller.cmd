13ce7d52aa4a5aa170687f6004c5025aa8a67123
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2023-03-27 20:28:59 +0200

    Update teams-uninstaller.cmd

--- Scripts/teams-uninstaller.cmd
+++ Scripts/teams-uninstaller.cmd
@@ -1,3 +1,5 @@
+echo off
+cls
 echo Hiding Teams From Taskbar
 Reg.exe add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "TaskbarMn" /t REG_DWORD /d "0" /f> nul
 echo Uninstalling Teams
