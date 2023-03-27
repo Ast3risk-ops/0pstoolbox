eca2c8ca97ac6258eac0a15e27bceb903a796c42
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2023-03-27 20:26:12 +0200

    Update teams-uninsatller.cmd

--- Scripts/teams-uninsatller.cmd
+++ Scripts/teams-uninsatller.cmd
@@ -1,2 +1,3 @@
 powershell -command "Get-AppxPackage MicrosoftTeams* | Remove-AppxPackage"> nul
 Reg.exe add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "TaskbarMn" /t REG_DWORD /d "0" /f> nul
+pause
