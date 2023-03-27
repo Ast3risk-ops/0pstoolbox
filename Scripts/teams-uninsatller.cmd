5aab472ec4387b3c69c4fcf880f82128638c23d9
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2023-03-27 20:26:56 +0200

    ah

--- Scripts/teams-uninsatller.cmd
+++
@@ -1,3 +0,0 @@
-powershell -command "Get-AppxPackage MicrosoftTeams* | Remove-AppxPackage"> nul
-Reg.exe add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "TaskbarMn" /t REG_DWORD /d "0" /f> nul
-pause
---
+++ Scripts/teams-uninstaller.cmd
@@ -0,0 +1,5 @@
+echo Hiding Teams From Taskbar
+Reg.exe add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "TaskbarMn" /t REG_DWORD /d "0" /f> nul
+echo Uninstalling Teams
+powershell -command "Get-AppxPackage MicrosoftTeams* | Remove-AppxPackage"> nul
+pause
