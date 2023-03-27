d39e45a1fa7bfce4f74cceaa5505eb799a704eda
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2023-03-27 21:38:06 +0200

    Update runtime2.ps1

--- Scripts/runtime2.ps1
+++ Scripts/runtime2.ps1
@@ -11,3 +11,6 @@ scoop bucket add extras | Out-Null
 Write-Output "Scoop is now installed"
 scoop install sudo aria2 wget git grep | Out-Null
 Write-Output "Sudo and Aria2 and Wget and Git is now installed"
+Install-Module PSWindowsUpdate
+Add-WUServiceManager -MicrosoftUpdate
+Write-Output "Windows update CLI have been installed"
